65709b472f337beb5c84cd126075b4bd9157975f
taty2010 <shydalys123@gmail.com>
2020-10-03 10:56:21 -0500

    search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,23 +1,23 @@
 def linear_search(arr, target):
-    # Your code here
-    for i in arr:
-        if i == target:
-            return arr[i]
-        elif target < i :
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-    mid = len(arr) - 1
-    for i in arr:
-        if mid == target:
-            return arr[l]
-    # Your code here
+    lhs = 0
+    rhs = len(arr) -1
+    while lhs <= rhs:
+        mid = (lhs + rhs) // 2
+        if target == arr[mid]:
+            return mid
+        elif arr[mid] > target:
+            rhs = mid - 1
+        else:
+            lhs = mid + 1
+    return -1
 
-
-    return -1  # not found
-
-arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
-def test_binary_search(arr1, -8):
+# arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+# def test_binary_search(arr1, -8):
